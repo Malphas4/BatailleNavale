b52b9ac1d4b38b8f84f8f45be91b704eca2e9060
malphas <victorweber57@gmail.com>
2023-10-18 08:50:59 +0200

    début COR, contenu_ de la classe expert

--- src/main/java/M1/reseau/COR/Expert.java
+++ src/main/java/M1/reseau/COR/Expert.java
@@ -1,5 +1,18 @@
 package M1.reseau.COR;
 
-public interface Expert {
-    +_suivant
+public class Expert {
+
+
+    public M1.reseau.COR.Expert _suivant = null;
+    public void  Suivant(M1.reseau.COR.Expert sv){
+        _suivant = sv;
+    }
+
+    public void traiter(String requete){
+        if(_suivant != null){
+            _suivant.traiter(requete);
+        }
+    }
 }
+
+

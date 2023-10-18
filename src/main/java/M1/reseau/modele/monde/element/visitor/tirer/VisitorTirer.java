6fd2af9b2f4bddc0eac7c145ef6830f0b758403f
RozenFR <iamzephir@gmail.com>
2023-10-18 12:13:38 +0200

    Modification visiteur pour les différents types de tirs

--- src/main/java/M1/reseau/modele/monde/element/visitor/tirer/VisitorTirer.java
+++ src/main/java/M1/reseau/modele/monde/element/visitor/tirer/VisitorTirer.java
@@ -2,8 +2,24 @@ package M1.reseau.modele.monde.element.visitor.tirer;
 
 import M1.reseau.modele.monde.element.ICase;
 import M1.reseau.modele.monde.element.visitor.IVisitorCase;
+import M1.reseau.modele.monde.grille.Grille;
 
 public class VisitorTirer implements IVisitorCase {
+
+    private Grille _grille;
+
+    public VisitorTirer(Grille _grille) {
+        set_grille(_grille);
+    }
+
+    public Grille get_grille() {
+        return _grille;
+    }
+
+    public void set_grille(Grille _grille) {
+        this._grille = _grille;
+    }
+
     @Override
     public void visite(ICase _case) {
 
--- src/main/java/M1/reseau/modele/monde/element/visitor/tirer/VisitorTirerCroix.java
+++ src/main/java/M1/reseau/modele/monde/element/visitor/tirer/VisitorTirerCroix.java
@@ -1,8 +1,14 @@
 package M1.reseau.modele.monde.element.visitor.tirer;
 
 import M1.reseau.modele.monde.element.ICase;
+import M1.reseau.modele.monde.grille.Grille;
 
 public class VisitorTirerCroix extends VisitorTirer {
+
+    public VisitorTirerCroix(Grille _grille) {
+        super(_grille);
+    }
+
     @Override
     public void visite(ICase _case) {
         super.visite(_case);

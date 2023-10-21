01497dcbaaaff0fd2572df29a4e35f5939855bf6
RozenFR <iamzephir@gmail.com>
2023-10-21 18:44:53 +0200

    Complétion des tests et ajout de classe

--- src/test/java/M1/reseau/modele/monde/element/fabrique/FabriqueCaseBateauTest.java
+++ src/test/java/M1/reseau/modele/monde/element/fabrique/FabriqueCaseBateauTest.java
@@ -1,25 +1,25 @@
 package M1.reseau.modele.monde.element.fabrique;
 
 import M1.reseau.modele.monde.element.ICase;
-import M1.reseau.modele.monde.element.type.CaseObstacle;
+import M1.reseau.modele.monde.element.type.CaseBateau;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FabriqueCaseObstacleTest {
+class FabriqueCaseBateauTest {
 
-    private FabriqueCaseObstacle _fabrique;
+    private FabriqueCaseBateau _fabrique;
 
     @BeforeEach
     void setUp() {
-        this._fabrique = new FabriqueCaseObstacle();
+        this._fabrique = new FabriqueCaseBateau();
     }
 
     @Test
     void testcreerCase() {
         ICase _case = this._fabrique.creerCase(1, 2);
-        assertInstanceOf(CaseObstacle.class, _case);
+        assertInstanceOf(CaseBateau.class, _case);
         assertEquals(1, _case.get_x());
         assertEquals(2, _case.get_y());
         assertFalse(_case.isPlacable());

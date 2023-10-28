3a79ad51d7f0c62258b762f4c027146d6074de99
Victor <victorweber57@gmail.com>
2023-10-28 11:25:42 +0200

    en commentaire car pas la bonne version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,23 @@
 plugins {
     id("java")
+    //id ("application")
+   // id ("org.openjfx.javafxplugin") version "0.0.9"
+   // id (org.beryx.jlink) version "2.12.0"
 }
 
 group = "M1.reseau"
 version = "1.0-SNAPSHOT"
 
+
+
 repositories {
     mavenCentral()
 }
+
+/*javafx {
+    version = "17"
+    modules("javafx.controls", "javafx.fxml")
+}*/
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
@@ -17,3 +27,5 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+

d8fbbad9b7969e1ce63382fe0b006c1eb8eab483
Victor <victorweber57@gmail.com>
2023-11-04 10:12:22 +0100

    squelettes des controleurs

--- src/main/java/M1/reseau/controleur/Controleur.java
+++ src/main/java/M1/reseau/controleur/Controleur.java
@@ -1,6 +1,14 @@
 package M1.reseau.controleur;
+import javafx.stage.Stage;
 
 public class Controleur {
    // @FXML
 
+    //in controller
+    /*public void setStage(Stage stage) {
+        this.stage = stage;
+        projectNameTextField.setOnAction(event -> {
+            stage.hide();
+        });
+    }*/
 }
--- src/main/java/M1/reseau/controleur/ControleurGrille.java
+++ src/main/java/M1/reseau/controleur/ControleurGrille.java
@@ -1,4 +1,50 @@
 package M1.reseau.controleur;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.TextFlow;
+
 public class ControleurGrille {
+
+    @FXML
+    private Button _btnAbandon;
+
+    @FXML
+    private Button _btnChatPartie;
+
+    @FXML
+    private Button _btnQuitter;
+
+    @FXML
+    private TextFlow _chatPartie;
+
+    @FXML
+    private TextArea _infoParties;
+
+    @FXML
+    private AnchorPane _panPartie;
+
+    @FXML
+    private TextField _textChatpartie;
+
+    @FXML
+    void Abandonner(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EnvoyerMessage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Quitter(ActionEvent event) {
+
+    }
+
 }

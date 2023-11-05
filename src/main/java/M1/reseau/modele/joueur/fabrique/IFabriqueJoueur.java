7f9bb2608b0fa608215d0b61dd8eecfb4c19aa38
RozenFR <iamzephir@gmail.com>
2023-11-05 19:56:33 +0100

    Suppression des fabriques inutiles

--- src/main/java/M1/reseau/modele/joueur/fabrique/IFabriqueJoueur.java
+++
@@ -1,8 +0,0 @@
-package M1.reseau.modele.joueur.fabrique;
-
-import M1.reseau.modele.joueur.IJoueur;
-import M1.reseau.modele.monde.grille.IGrille;
-
-public interface IFabriqueJoueur {
-    IJoueur creerJoueur(String _pseudo, IGrille _grille);
-}
--- src/main/java/M1/reseau/modele/monde/element/fabrique/IFabriqueCase.java
+++
@@ -1,7 +0,0 @@
-package M1.reseau.modele.monde.element.fabrique;
-
-import M1.reseau.modele.monde.element.ICase;
-
-public interface IFabriqueCase {
-    ICase creerCase(int _x, int _y);
-}

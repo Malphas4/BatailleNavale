33c278d34196e6d0ed585edafb8ba2496e72e4cc
Victor <victorweber57@gmail.com>
2023-11-07 18:33:17 +0100

    chargement FXML dans main, liens avec le controleur pour chaque FXML et essais infructueux pour gérer l'affichage de multiples fenetres et probelme pour specifier le path de javafx réglé

--- src/main/java/M1/reseau/Main.java
+++ src/main/java/M1/reseau/Main.java
@@ -1,8 +1,18 @@
 package M1.reseau;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+
+import java.io.IOException;
+
+import M1.reseau.controleur.*;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
     public static void main(String[] args) {
         // Press Alt+Entrée with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
@@ -21,10 +31,98 @@ public class Main {
         try {
             // Testclient.unClient();
             //   Testserveur.unServeur();
+            launch();
 
         } catch (Exception e) {
            // e.printStackTrace();
+
         }
 
     }
+
+
+
+    @Override
+    public void start(Stage stage){
+        // Leaving this line for reference
+        //FrontController frontController = FrontController.getInstance();
+
+
+
+        try {
+            // loader pour les FXML et liens avec les controleurs
+            // possibilité de factorisation mais pas fait
+
+
+            FXMLLoader menuLoader = new FXMLLoader(getClass().getResource("menu.fxml"));
+            Parent nodeMenu=menuLoader.load();
+            ControleurMenu ControleurMenu = menuLoader.getController();
+
+
+            FXMLLoader pseudoLoader = new FXMLLoader(getClass().getResource("pseudo.fxml"));
+            Parent nodePseudo=pseudoLoader.load();
+            ControleurPseudo ControleurPseudo = pseudoLoader.getController();
+
+            FXMLLoader attenteLoader = new FXMLLoader(getClass().getResource("attente.fxml"));
+            Parent nodeAttente=attenteLoader.load();
+            ControleurAttente ControleurAttente = attenteLoader.getController();
+
+            FXMLLoader lobbyLoader = new FXMLLoader(getClass().getResource("lobby.fxml"));
+            Parent nodeLobby=lobbyLoader.load();
+            ControleurLobby ControleurLobby = menuLoader.getController();
+
+            FXMLLoader grilleLoader = new FXMLLoader(getClass().getResource("grille.fxml"));
+            Parent nodeGrille=grilleLoader.load();
+            ControleurGrille ControleurGrille = grilleLoader.getController();
+
+            //initialisation des modeles
+            //            DataModel model = new DataModel();
+            //            ControleurMenu.initModel(model);
+            //            ControleurPseudo.initModel(model);
+            //            menuController.initModel(model);
+
+
+            //generation de la fenetre
+            //            Scene scene = new Scene(root, 800, 600);
+                        stage.setScene(new Scene(nodeMenu));
+                        stage.show();
+
+
+
+
+            // ici Borderpan et pan classiques mis dedans
+            // BorderPane root = new BorderPane();
+           /* code de l'exemple A SUPPR
+           FXMLLoader listLoader = new FXMLLoader(getClass().getResource("list.fxml"));
+            root.setCenter(listLoader.load());
+            ListController listController = listLoader.getController();
+
+            //init controleurs & model
+            DataModel model = new DataModel();
+            listController.initModel(model);
+            editorController.initModel(model);
+            menuController.initModel(model);
+
+            Scene scene = new Scene(root, 800, 600);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            */
+
+
+            ///
+
+
+            //root.setCenter(listLoader.load());
+            //ListController listController = listLoader.getController();
+
+
+
+            // load can throw an IOException if their is an issue with the FXML file
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }

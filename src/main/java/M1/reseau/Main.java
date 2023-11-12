e5140552c1552a44dcfa458b360378807a0316e9
Victor <victorweber57@gmail.com>
2023-11-12 15:04:04 +0100

    erreur fxml javafx 21 enlevé

--- src/main/java/M1/reseau/Main.java
+++ src/main/java/M1/reseau/Main.java
@@ -15,20 +15,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+8.
-            System.out.println("i = " + i);
-        }
        // client  Testclient = new client();
        // serveur Testserveur = new serveur();
-
         try {
             // Testclient.unClient();
             //   Testserveur.unServeur();
@@ -45,20 +33,11 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception{
-        // Leaving this line for reference
-        //FrontController frontController = FrontController.getInstance();
-
-
-
-       //try {
-            // loader pour les FXML et liens avec les controleurs
-            // possibilité de factorisation mais pas fait
 
 
             FXMLLoader menuLoader = new FXMLLoader(getClass().getResource("/menuv2.fxml"));
             Scene sceneMenu = (Scene) menuLoader.load();
 
-             //Parent nodeMenu=(Parent)menuLoader.load();
             ControleurMenu ControleurMenu = menuLoader.getController();
 
             FXMLLoader pseudoLoader = new FXMLLoader(getClass().getResource("/pseudo.fxml"));
@@ -79,6 +58,7 @@ public class Main extends Application{
             ControleurGrille ControleurGrille = grilleLoader.getController();
 
             //initialisation des modeles
+            //fonction
             //            DataModel model = new DataModel();
             //            ControleurMenu.initModel(model);
             //            ControleurPseudo.initModel(model);
@@ -93,15 +73,7 @@ public class Main extends Application{
             stage.show();
 
 
-
-
-            // ici Borderpan et pan classiques mis dedans
-            // BorderPane root = new BorderPane();
-           /* code de l'exemple A SUPPR
-           FXMLLoader listLoader = new FXMLLoader(getClass().getResource("list.fxml"));
-            root.setCenter(listLoader.load());
-            ListController listController = listLoader.getController();
-
+            /*
             //init controleurs & model
             DataModel model = new DataModel();
             listController.initModel(model);
@@ -113,18 +85,6 @@ public class Main extends Application{
             primaryStage.show();
              */
 
-
-
-
-            ///
-
-
-            //root.setCenter(listLoader.load());
-            //ListController listController = listLoader.getController();
-
-
-
-            // load can throw an IOException if their is an issue with the FXML file
 
        /* } catch (IOException e) {
             e.printStackTrace();

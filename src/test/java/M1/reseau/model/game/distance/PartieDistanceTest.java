4eed544f83545cad4325373e0bfac871ed12d30f
RozenFR <iamzephir@gmail.com>
2023-11-12 15:45:07 +0100

    - Implémentation des tests

--- src/test/java/M1/reseau/model/game/distance/PartieDistanceTest.java
+++ src/test/java/M1/reseau/model/game/distance/PartieDistanceTest.java
@@ -1,52 +1,235 @@
 package M1.reseau.model.game.distance;
 
 import M1.reseau.model.exception.IJoueurException;
+import M1.reseau.model.exception.IPartieException;
 import M1.reseau.model.player.classic.JoueurNormal;
 import M1.reseau.model.world.grid.Grille;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class PartieDistanceTest {
 
-    private PartieDistance _partie;
+    private PartieDistance _partieVide;
+    private PartieDistance _partiePleine;
 
     @BeforeEach
     void setUp() {
-        _partie = new PartieDistance(8, 8);
-    }
+        /* Initialisation de la partie vide */
+        _partieVide = new PartieDistance(8, 8);
 
-    @Test
-    void set_joueur() {
+        /*Initialisation de la partie pleine */
+        _partiePleine = new PartieDistance(8, 8);
         try {
-            JoueurNormal _joueur = new JoueurNormal("Joueur",
-                                                    (Grille) _partie.get_fabriqueGrille().creerGrille(),
-                                                    (Grille) _partie.get_fabriqueGrille().creerGrille());
-        } catch (IJoueurException e) {
+            _partiePleine.ajouterJoueur("Joueur 1");
+        } catch (IPartieException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Test
-    void set_fabriqueGrille() {
+    void constructeurTest() {
+        /* Vérification que partie n'est pas null */
+        assertNotNull(_partieVide);
+
+        /* Vérification des attributs */
+        assertNull(_partieVide.get_joueur());
+        assertFalse(_partieVide.is_aGagner());
+        assertFalse(_partieVide.is_tourJoueur());
     }
 
     @Test
-    void commencer() {
+    void set_joueurTest() {
+        /* Initialisation avec Partie Vide */
+
+        /* Test de la méthode */
+        try {
+            JoueurNormal _joueur = new JoueurNormal("Joueur 1",
+                                                    (Grille) _partieVide.get_fabriqueGrille().creerGrille(),
+                                                    (Grille) _partieVide.get_fabriqueGrille().creerGrille());
+            _partieVide.set_joueur(_joueur);
+            assertNotNull(_partieVide.get_joueur());
+            assertEquals("Joueur 1", _partieVide.get_joueur().get_pseudo());
+        } catch (IJoueurException e) {
+            fail();
+        }
     }
 
     @Test
-    void tourSuivant() {
+    void set_joueurErreurNullTest() {
+        /* Initialisation avec Partie Vide */
+        JoueurNormal _joueur = null;
+
+        /* Test de la méthode */
+        try {
+            _partieVide.set_joueur(_joueur);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertNull(_partieVide.get_joueur());
+        }
     }
 
     @Test
-    void getJoueurCourant() {
+    void commencerTest() {
+        /* Initialisation avec partie pleine */
+
+        /* On test la méthode */
+        try {
+            _partiePleine.commencer();
+            assertTrue(_partiePleine.is_commence());
+        } catch (IPartieException e) {
+            fail();
+        }
+    }
+
+    @Test
+    void commencerErreurJoueurNullTest() {
+        /* Initialisation avec partie vide */
+
+        /* On test la méthode */
+        try {
+            _partieVide.commencer();
+            fail();
+        } catch (IPartieException e) {
+            assertFalse(_partieVide.is_commence());
+        }
+    }
+
+    @Test
+    void commencerErreurCommenceTest() {
+        /* Initialisation avec partie pleine */
+        _partiePleine.set_commence(true);
+
+        /* On test la méthode */
+        try {
+            _partiePleine.commencer();
+            fail();
+        } catch (IPartieException e) {
+            assertTrue(_partiePleine.is_commence());
+        }
+    }
+
+    @Test
+    void commencerErreurTermineTest() {
+        /* Initialisation avec partie pleine */
+        _partiePleine.set_termine(true);
+
+        /* On test la méthode */
+        try {
+            _partiePleine.commencer();
+            fail();
+        } catch (IPartieException e) {
+            assertFalse(_partiePleine.is_commence());
+        }
+    }
+
+    @Test
+    void tourSuivantTest() {
+        /* Initialisation avec partie pleine */
+        _partiePleine.set_commence(true);
+
+        /* On test la méthode */
+        try {
+            _partiePleine.tourSuivant();
+            assertEquals(1, _partiePleine.get_nbTour());
+            assertTrue(_partiePleine.is_tourJoueur());
+        } catch (IPartieException e) {
+            fail();
+        }
+    }
+
+    @Test
+    void tourSuivantErreurCommenceTest() {
+        /* Initialisation avec partie pleine */
+
+        /* On test la méthode */
+        try {
+            _partiePleine.tourSuivant();
+            fail();
+        } catch (IPartieException e) {
+            assertEquals(0, _partiePleine.get_nbTour());
+        }
+    }
+
+    @Test
+    void tourSuivantErreurTermineTest() {
+        /* Initialisation avec partie pleine */
+        _partiePleine.set_commence(true);
+        _partiePleine.set_termine(true);
+
+        /* Test de la méthode */
+        try {
+            _partiePleine.tourSuivant();
+            fail();
+        } catch (IPartieException e) {
+            assertEquals(0, _partiePleine.get_nbTour());
+        }
+    }
+
+    @Test
+    void tourSuivantErreurJoueurNullTest() {
+        /* Test sur partie vide */
+        _partieVide.set_commence(true);
+        _partieVide.set_termine(true);
+
+        /* Test de la méthode */
+        try {
+            _partieVide.tourSuivant();
+            fail();
+        } catch (IPartieException e) {
+            assertEquals(0, _partieVide.get_nbTour());
+        }
+    }
+
+    @Test
+    void getJoueurCourantTest() {
+        /* Initialisation de Partie J12 */
+        _partiePleine.set_commence(true);
+        _partiePleine.set_tourJoueur(true);
+
+        /* Vérification que le joueur courant est le joueur 1 */
+        try {
+            assertEquals(_partiePleine.get_joueur(), _partiePleine.getJoueurCourant());
+        } catch (IPartieException e) {
+            fail();
+        }
+
+        /* On passe le tour */
+        try {
+            _partiePleine.tourSuivant();
+        } catch (IPartieException e) {
+            fail();
+        }
+
+        /* Vérification que le joueur courant retourne une erreur */
+        JoueurNormal _joueur = null;
+        try {
+            _joueur = (JoueurNormal) _partiePleine.getJoueurCourant();
+            fail();
+        } catch (IPartieException e) {
+            assertNull(_joueur);
+        }
+    }
+
+    @Test
+    void getJoueurCourantErreurJoueurNullTest() {
+        /* Test de la méthode avec Partie Vide */
+        try {
+            _partieVide.getJoueurCourant();
+            fail();
+        } catch (IPartieException e) {
+            assertNull(_partieVide.get_joueur());
+        }
     }
 
     @Test
     void ajouterJoueur() {
+
     }
 
     @Test
     void fin() {
+
     }
 }
--- src/test/java/M1/reseau/model/game/local/PartieLocaleTest.java
+++ src/test/java/M1/reseau/model/game/local/PartieLocaleTest.java
@@ -12,28 +12,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PartieLocaleTest {
 
-    private PartieLocale _partieLocaleVide;
-    private PartieLocale _partieLocaleJ1;
-    private PartieLocale _partieLocaleJ12;
+    private PartieLocale _partieVide;
+    private PartieLocale _partieJ1;
+    private PartieLocale _partieJ12;
 
     @BeforeEach
     void setUp() {
         /* Initialisation d'une partie à vide */
-        _partieLocaleVide = new PartieLocale(8, 8);
+        _partieVide = new PartieLocale(8, 8);
 
         /* Initialisation d'une partie avec 1 joueur */
-        _partieLocaleJ1 = new PartieLocale(8, 8);
+        _partieJ1 = new PartieLocale(8, 8);
         try {
-            _partieLocaleJ1.ajouterJoueur("Joueur 1");
+            _partieJ1.ajouterJoueur("Joueur 1");
         } catch (IPartieException e) {
             throw new RuntimeException(e);
         }
 
         /* Initialisation d'une partie avec 2 joueurs */
-        _partieLocaleJ12 = new PartieLocale(8, 8);
+        _partieJ12 = new PartieLocale(8, 8);
         try {
-            _partieLocaleJ12.ajouterJoueur("Joueur 1");
-            _partieLocaleJ12.ajouterJoueur("Joueur 2");
+            _partieJ12.ajouterJoueur("Joueur 1");
+            _partieJ12.ajouterJoueur("Joueur 2");
         } catch (IPartieException e) {
             throw new RuntimeException(e);
         }
@@ -42,34 +42,34 @@ class PartieLocaleTest {
     @Test
     void constructeurTest() {
         /* Vérification que partie n'est pas null */
-        assertNotNull(_partieLocaleVide);
+        assertNotNull(_partieVide);
 
         /* Vérification des attributs */
         // Vérification pour les joueurs
-        assertNull(_partieLocaleVide.get_joueur1());
-        assertNull(_partieLocaleVide.get_joueur2());
-        assertFalse(_partieLocaleVide.is_aGagner1());
-        assertFalse(_partieLocaleVide.is_aGagner2());
+        assertNull(_partieVide.get_joueur1());
+        assertNull(_partieVide.get_joueur2());
+        assertFalse(_partieVide.is_aGagner1());
+        assertFalse(_partieVide.is_aGagner2());
         // Vérification de la classe parente
-        assertEquals(0, _partieLocaleVide.get_nbTour());
-        assertFalse(_partieLocaleVide.is_commence());
-        assertFalse(_partieLocaleVide.is_pause());
-        assertFalse(_partieLocaleVide.is_termine());
+        assertEquals(0, _partieVide.get_nbTour());
+        assertFalse(_partieVide.is_commence());
+        assertFalse(_partieVide.is_pause());
+        assertFalse(_partieVide.is_termine());
         // Vérification de la grille
-        assertNotNull(_partieLocaleVide.get_fabriqueGrille());
-        assertEquals(new FabriqueGrille(8, 8), _partieLocaleVide.get_fabriqueGrille());
+        assertNotNull(_partieVide.get_fabriqueGrille());
+        assertEquals(new FabriqueGrille(8, 8), _partieVide.get_fabriqueGrille());
     }
 
     @Test
     void set_joueur1Test() {
         /* On ajoute un joueur 1 sur la partie vide */
         try {
-            _partieLocaleVide.set_joueur1(new JoueurNormal(
+            _partieVide.set_joueur1(new JoueurNormal(
                     "Joueur 1",
-                    (Grille) _partieLocaleVide.get_fabriqueGrille().creerGrille(),
-                    (Grille) _partieLocaleVide.get_fabriqueGrille().creerGrille()
+                    (Grille) _partieVide.get_fabriqueGrille().creerGrille(),
+                    (Grille) _partieVide.get_fabriqueGrille().creerGrille()
             ));
-            assertNotNull(_partieLocaleVide.get_joueur1());
+            assertNotNull(_partieVide.get_joueur1());
         } catch (IJoueurException e) {
             fail();
         }
@@ -79,10 +79,10 @@ class PartieLocaleTest {
     void set_joueur1ErreurNullTest() {
         /* On ajoute un joueur null sur la partie vide */
         try {
-            _partieLocaleVide.set_joueur1(null);
+            _partieVide.set_joueur1(null);
             fail();
         } catch (IllegalArgumentException e) {
-            assertNull(_partieLocaleVide.get_joueur1());
+            assertNull(_partieVide.get_joueur1());
         }
     }
 
@@ -90,12 +90,12 @@ class PartieLocaleTest {
     void set_joueur2Test() {
         /* On ajoute un joueur 2 sur la partie vide */
         try {
-            _partieLocaleVide.set_joueur2(new JoueurNormal(
+            _partieVide.set_joueur2(new JoueurNormal(
                     "Joueur 2",
-                    (Grille) _partieLocaleVide.get_fabriqueGrille().creerGrille(),
-                    (Grille) _partieLocaleVide.get_fabriqueGrille().creerGrille()
+                    (Grille) _partieVide.get_fabriqueGrille().creerGrille(),
+                    (Grille) _partieVide.get_fabriqueGrille().creerGrille()
             ));
-            assertNotNull(_partieLocaleVide.get_joueur2());
+            assertNotNull(_partieVide.get_joueur2());
         } catch (IJoueurException e) {
             fail();
         }
@@ -106,10 +106,10 @@ class PartieLocaleTest {
         /* On ajoute un joueur null sur la partie vide */
         /* On test la méthode */
         try {
-            _partieLocaleVide.set_joueur2(null);
+            _partieVide.set_joueur2(null);
             fail();
         } catch (IllegalArgumentException e) {
-            assertNull(_partieLocaleVide.get_joueur2());
+            assertNull(_partieVide.get_joueur2());
         }
     }
 
@@ -119,8 +119,8 @@ class PartieLocaleTest {
 
         /* On test la méthode */
         try {
-            _partieLocaleJ12.commencer();
-            assertTrue(_partieLocaleJ12.is_commence());
+            _partieJ12.commencer();
+            assertTrue(_partieJ12.is_commence());
         } catch (IPartieException e) {
             fail();
         }
@@ -132,50 +132,50 @@ class PartieLocaleTest {
 
         /* On test la méthode */
         try {
-            _partieLocaleJ1.commencer();
+            _partieJ1.commencer();
             fail();
         } catch (IPartieException e) {
-            assertFalse(_partieLocaleJ1.is_commence());
+            assertFalse(_partieJ1.is_commence());
         }
     }
 
     @Test
     void commencerErreurCommenceTest() {
         /* On test partie avec 2 joueurs */
-        _partieLocaleJ12.set_commence(true);
+        _partieJ12.set_commence(true);
 
         /* On test la méthode */
         try {
-            _partieLocaleJ12.commencer();
+            _partieJ12.commencer();
             fail();
         } catch (IPartieException e) {
-            assertTrue(_partieLocaleJ12.is_commence());
+            assertTrue(_partieJ12.is_commence());
         }
     }
 
     @Test
     void commencerErreurTermineTest() {
         /* On test partie avec 2 joueurs */
-        _partieLocaleJ12.set_termine(true);
+        _partieJ12.set_termine(true);
 
         /* On test la méthode */
         try {
-            _partieLocaleJ12.commencer();
+            _partieJ12.commencer();
             fail();
         } catch (IPartieException e) {
-            assertFalse(_partieLocaleJ12.is_commence());
+            assertFalse(_partieJ12.is_commence());
         }
     }
 
     @Test
     void tourSuivantTest() {
         /* On initialise partie j12 */
-        _partieLocaleJ12.set_commence(true);
+        _partieJ12.set_commence(true);
 
         /* On test la méthode */
         try {
-            _partieLocaleJ12.tourSuivant();
-            assertEquals(1, _partieLocaleJ12.get_nbTour());
+            _partieJ12.tourSuivant();
+            assertEquals(1, _partieJ12.get_nbTour());
         } catch (IPartieException e) {
             fail();
         }
@@ -185,65 +185,65 @@ class PartieLocaleTest {
     void tourSuivantErreurCommenceTest() {
         /* On test la méthode */
         try {
-            _partieLocaleJ12.tourSuivant();
+            _partieJ12.tourSuivant();
             fail();
         } catch (IPartieException e) {
-            assertEquals(0, _partieLocaleJ12.get_nbTour());
+            assertEquals(0, _partieJ12.get_nbTour());
         }
     }
 
     @Test
     void tourSuivantErreurTermineTest() {
         /* On initialise la partie J12 */
-        _partieLocaleJ12.set_commence(true);
-        _partieLocaleJ12.set_termine(true);
+        _partieJ12.set_commence(true);
+        _partieJ12.set_termine(true);
 
         /* Test de la méthode */
         try {
-            _partieLocaleJ12.tourSuivant();
+            _partieJ12.tourSuivant();
             fail();
         } catch (IPartieException e) {
-            assertEquals(0, _partieLocaleJ12.get_nbTour());
+            assertEquals(0, _partieJ12.get_nbTour());
         }
     }
 
     @Test
     void tourSuivantErreurJoueurNullTest() {
         /* Test sur partie vide */
-        _partieLocaleVide.set_commence(true);
-        _partieLocaleVide.set_termine(true);
+        _partieVide.set_commence(true);
+        _partieVide.set_termine(true);
 
         /* Test de la méthode */
         try {
-            _partieLocaleVide.tourSuivant();
+            _partieVide.tourSuivant();
             fail();
         } catch (IPartieException e) {
-            assertEquals(0, _partieLocaleVide.get_nbTour());
+            assertEquals(0, _partieVide.get_nbTour());
         }
     }
 
     @Test
     void getJoueurCourantTest() {
         /* Initialisation de Partie J12 */
-        _partieLocaleJ12.set_commence(true);
+        _partieJ12.set_commence(true);
 
         /* Vérification que le joueur courant est le joueur 1 */
         try {
-            assertEquals(_partieLocaleJ12.get_joueur1(), _partieLocaleJ12.getJoueurCourant());
+            assertEquals(_partieJ12.get_joueur1(), _partieJ12.getJoueurCourant());
         } catch (IPartieException e) {
             fail();
         }
 
         /* On passe le tour */
         try {
-            _partieLocaleJ12.tourSuivant();
+            _partieJ12.tourSuivant();
         } catch (IPartieException e) {
             fail();
         }
 
         /* Vérification que le joueur courant est le joueur 1 */
         try {
-            assertEquals(_partieLocaleJ12.get_joueur2(), _partieLocaleJ12.getJoueurCourant());
+            assertEquals(_partieJ12.get_joueur2(), _partieJ12.getJoueurCourant());
         } catch (IPartieException e) {
             fail();
         }
@@ -253,18 +253,18 @@ class PartieLocaleTest {
     void getJoueurCourantErreurJoueurNullTest() {
         /* Test de la méthode avec Partie Vide */
         try {
-            _partieLocaleVide.getJoueurCourant();
+            _partieVide.getJoueurCourant();
             fail();
         } catch (IPartieException e) {
-            assertNull(_partieLocaleVide.get_joueur1());
+            assertNull(_partieVide.get_joueur1());
         }
 
         /* Test de la méthode avec Partie J1 */
         try {
-            _partieLocaleJ1.getJoueurCourant();
+            _partieJ1.getJoueurCourant();
             fail();
         } catch (IPartieException e) {
-            assertNull(_partieLocaleJ1.get_joueur2());
+            assertNull(_partieJ1.get_joueur2());
         }
     }
 
@@ -274,23 +274,23 @@ class PartieLocaleTest {
         /* Test de la méthode */
         // Ajout du joueur 1
         try {
-            _partieLocaleVide.ajouterJoueur("Joueur 1");
-            assertNotNull(_partieLocaleVide.get_joueur1());
-            assertNull(_partieLocaleVide.get_joueur2());
+            _partieVide.ajouterJoueur("Joueur 1");
+            assertNotNull(_partieVide.get_joueur1());
+            assertNull(_partieVide.get_joueur2());
         } catch (IPartieException e) {
             fail();
         }
 
         // Ajout du joueur 2
         try {
-            _partieLocaleVide.ajouterJoueur("Joueur 2");
-            assertNotNull(_partieLocaleVide.get_joueur2());
+            _partieVide.ajouterJoueur("Joueur 2");
+            assertNotNull(_partieVide.get_joueur2());
         } catch (IPartieException e) {
             fail();
         }
 
-        assertEquals("Joueur 1", _partieLocaleVide.get_joueur1().get_pseudo());
-        assertEquals("Joueur 2", _partieLocaleVide.get_joueur2().get_pseudo());
+        assertEquals("Joueur 1", _partieVide.get_joueur1().get_pseudo());
+        assertEquals("Joueur 2", _partieVide.get_joueur2().get_pseudo());
     }
 
     @Test
@@ -298,11 +298,11 @@ class PartieLocaleTest {
         /* Initialisation avec Partie J12 */
         /* Test de la méthode */
         try {
-            _partieLocaleJ12.ajouterJoueur("Joueur 3");
+            _partieJ12.ajouterJoueur("Joueur 3");
             fail();
         } catch (IPartieException e) {
-            assertEquals("Joueur 1", _partieLocaleJ12.get_joueur1().get_pseudo());
-            assertEquals("Joueur 2", _partieLocaleJ12.get_joueur2().get_pseudo());
+            assertEquals("Joueur 1", _partieJ12.get_joueur1().get_pseudo());
+            assertEquals("Joueur 2", _partieJ12.get_joueur2().get_pseudo());
         }
     }
 
@@ -310,26 +310,26 @@ class PartieLocaleTest {
     void finTest() {
         /* Initialisation avec Partie J12 */
         /* joueur 1 a gagné */
-        _partieLocaleJ12.set_commence(true);
-        _partieLocaleJ12.set_aGagner1(true);
+        _partieJ12.set_commence(true);
+        _partieJ12.set_aGagner1(true);
 
         /* Test de la méthode */
         try {
-            _partieLocaleJ12.fin();
-            assertTrue(_partieLocaleJ12.is_termine());
+            _partieJ12.fin();
+            assertTrue(_partieJ12.is_termine());
         } catch (IPartieException e) {
             fail();
         }
 
         /* joueur 2 a gagné */
-        _partieLocaleJ12.set_termine(false);
-        _partieLocaleJ12.set_aGagner1(false);
-        _partieLocaleJ12.set_aGagner2(true);
+        _partieJ12.set_termine(false);
+        _partieJ12.set_aGagner1(false);
+        _partieJ12.set_aGagner2(true);
 
         /* Test de la méthode */
         try {
-            _partieLocaleJ12.fin();
-            assertTrue(_partieLocaleJ12.is_termine());
+            _partieJ12.fin();
+            assertTrue(_partieJ12.is_termine());
         } catch (IPartieException e) {
             fail();
         }
@@ -341,30 +341,40 @@ class PartieLocaleTest {
         /* Initialisation de Partie J12 */
         /* Test de la méthode */
         try {
-            _partieLocaleJ12.fin();
+            _partieJ12.fin();
             fail();
         } catch (IPartieException e) {
-            assertFalse(_partieLocaleJ12.is_termine());
+            assertFalse(_partieJ12.is_termine());
         }
     }
 
     @Test
     void finErreurTermineTest() {
         /* Initialisation de Partie J12 */
-        _partieLocaleJ12.set_commence(true);
-        _partieLocaleJ12.set_termine(true);
+        _partieJ12.set_commence(true);
+        _partieJ12.set_termine(true);
 
         /* Test de la méthode */
         try {
-            _partieLocaleJ12.fin();
+            _partieJ12.fin();
             fail();
         } catch (IPartieException e) {
-            assertTrue(_partieLocaleJ12.is_termine());
+            assertTrue(_partieJ12.is_termine());
         }
     }
 
     @Test
     void finErreurGagneTest() {
+        /* Initialisation de Partie J12 */
+        _partieJ12.set_commence(true);
 
+        /* Test de la méthode */
+        /* Test de la méthode */
+        try {
+            _partieJ12.fin();
+            fail();
+        } catch (IPartieException e) {
+            assertFalse(_partieJ12.is_termine());
+        }
     }
 }

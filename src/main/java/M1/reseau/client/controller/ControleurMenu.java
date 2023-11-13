8660d00e08fee2ebf11ec897953c76f27d237382
malphas <victorweber57@gmail.com>
2023-11-13 11:51:25 +0100

    base Salon

--- src/main/java/M1/reseau/client/controller/ControleurMenu.java
+++ src/main/java/M1/reseau/client/controller/ControleurMenu.java
@@ -1,13 +1,19 @@
 package M1.reseau.client.controller;
 
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-    public class ControleurMenu {
+import java.io.IOException;
+
+public class ControleurMenu {
 
         @FXML
         private AnchorPane Menu;
@@ -25,14 +31,31 @@ import javafx.stage.Stage;
         private Stage _menuDemarage;
 
         @FXML
-        void accesLobby(ActionEvent event) {
+        void accesLobby(ActionEvent event)throws IOException {
             System.out.print("Aller au Lobby\n");
+
+                FXMLLoader lobbyLoader = new FXMLLoader(getClass().getResource("/lobby.fxml"));
+                Scene sceneLobby = lobbyLoader.load();
+                //ControleurLobby _controleurLobby = lobbyLoader.getController();
+                Stage stageActuel = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stageActuel.setScene(sceneLobby);
+                stageActuel.show();
+
 
         }
 
         @FXML
-        void accesLocal(ActionEvent event) {
+        void accesLocal(ActionEvent event) throws IOException{
             System.out.print("Jouer en local\n");
+            FXMLLoader grilleLoader = new FXMLLoader(getClass().getResource("/grilleV2.fxml"));
+           // AnchorPane grille=grilleLoader.load();
+           // Scene sceneGrille = new Scene(grille);
+             Scene sceneGrille=grilleLoader.load();
+
+            //ControleurMenu ControleurGrille = grilleLoader.getController();
+            Stage stageActuel = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stageActuel.setScene(sceneGrille);
+            stageActuel.show();
 
 
         }
@@ -40,6 +63,8 @@ import javafx.stage.Stage;
         @FXML
         void quitter(ActionEvent event) {
             System.out.print("au revoir\n");
+            Platform.exit();
+            System.exit(0);
 
 
         }
--- src/main/java/M1/reseau/client/controller/ControleurPseudo.java
+++ src/main/java/M1/reseau/client/controller/ControleurPseudo.java
@@ -3,10 +3,16 @@ package M1.reseau.client.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ControleurPseudo {
 
@@ -29,8 +35,21 @@ public class ControleurPseudo {
     private Pane _panePseudo;
 
     @FXML
-    void validationPseudo(ActionEvent event) {
+    void validationPseudo(ActionEvent event)throws IOException {
         System.out.print("connect\n");
+        if( _champPseudo.getText().trim().isEmpty()){
+            _msgErreur.setText("Veuillez entrer un pseudo");
+        }
+        else{
+            System.out.print("connection pseudo : "+_champPseudo.getText()+"\n");
+
+            FXMLLoader menuLoader = new FXMLLoader(getClass().getResource("/menuv2.fxml"));
+            Scene sceneGrille = menuLoader.load();
+            //ControleurMenu _controleurMenu = menuLoader.getController();
+            Stage stageActuel = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stageActuel.setScene(sceneGrille);
+            stageActuel.show();
+        }
 
 
     }

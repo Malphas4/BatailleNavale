85ef31163b388c4fe233061b1c3987c149f671d1
Victor <victorweber57@gmail.com>
2023-11-14 01:28:19 +0100

    client UDP bof, le préceden était mieux mais il compile pour tester avec l'application

--- src/main/java/M1/reseau/client/ClientUDP.java
+++ src/main/java/M1/reseau/client/ClientUDP.java
@@ -1,6 +1,6 @@
 package M1.reseau.client;
 
-import M1.reseau.serveur.ServeurUDP;
+import M1.reseau.serveur.serveur.ServeurUDP;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -11,26 +11,13 @@ public class ClientUDP {
 
     public static void main(String argv[]) throws Exception
     {
-        InetAddress serveur = InetAddress.getByName(argv[0]);
-        int length = argv[1].length();
-        byte buffer[] = argv[1].getBytes();
-        DatagramPacket dataSent = new DatagramPacket(buffer,length,serveur, ServeurUDP.port);
-        DatagramSocket socket = new DatagramSocket();
-        InetAddress adr;
-        DatagramPacket packet;
-        // adr contient l'@IP de la partie serveur
-        adr = InetAddress.getByName("localhost");
-        // données à envoyer : chaîne de caractères
+        InetAddress serveur = InetAddress.getByName("localhost");
         byte[] data = (new String("youpi")).getBytes();
-        // création du paquet avec les données et en précisant l'adresse du serveur
-        // (@IP et port sur lequel il écoute : 7777)
-        packet = new DatagramPacket(data, data.length, adr, 7777);
-        // création d'une socket, sans la lier à un port particulier
-        socket = new DatagramSocket();
-        // envoi du paquet via la socket
-        socket.send(packet);
+
+        DatagramPacket dataSent = new DatagramPacket(data,data.length,serveur,7777 );
+        DatagramSocket socket = new DatagramSocket();
         socket.send(dataSent);
-        DatagramPacket dataRecieved = new DatagramPacket(new byte[length],length);
+        DatagramPacket dataRecieved = new DatagramPacket(new byte[taille],taille);
         socket.receive(dataRecieved);
         System.out.println("Data recieved : " + new String(dataRecieved.getData()));
         System.out.println("From : " + dataRecieved.getAddress() + ":" + dataRecieved.getPort());

2c36eadf1e3bbbb715b9edb4306fd4a3112912ef
malphas <victorweber57@gmail.com>
2023-11-14 09:50:38 +0100

    correction erreurs dans SingletonUDP, fonctonnel

--- src/main/java/M1/reseau/serveur/singletons/SingletonUDP.java
+++ src/main/java/M1/reseau/serveur/singletons/SingletonUDP.java
@@ -1,32 +1,40 @@
 package M1.reseau.serveur.singletons;
 
-import M1.reseau.serveur.serveur.ServeurUDP;
-
 import java.io.IOException;
 import java.net.*;
 
+public class SingletonUDP {
 
-public class SingletonUDP
-{
+    //instance
+    private static SingletonUDP _singletonUDP;
 
-    /** Constructeur privé
-     un seul socket UDP est nécessaire pour le serveur
-     */
-    final DatagramSocket socketUDP;
-    final  int _taille = 1024;
+    /*
+    * Données de l'instance du socket UDP
+    * */
+    final DatagramSocket socketUDP;//= new DatagramSocket();
+    final int _taille = 1024;
     final byte[] _buffer = new byte[_taille];
-    final int _port;
-    InetAddress _adr ;
-    private SingletonUDP( int port,String ad) throws UnknownHostException, SocketException {
-        _port=7777;
-        InetAddress _adr= InetAddress.getByName("localhost");
+    int _port = 7777;
+    ;
+    InetAddress _adr= InetAddress.getByName("localhost");;
+
+    /**
+     * Constructeur privé
+     * un seul socket UDP est nécessaire pour le serveur
+     */
+    private SingletonUDP(int port, String ad) throws UnknownHostException, SocketException {
+        _port = 7777;
+        InetAddress _adr = InetAddress.getByName(ad);
         // création d'une socket, sans la lier à un port particulier
         this.socketUDP = new DatagramSocket();
 
     }
+    /**
+     * Constructeur privé par défaut
+     * un seul socket UDP est nécessaire pour le serveur
+     */
     private SingletonUDP() throws UnknownHostException, SocketException {
-        _port=7777;
-        InetAddress _adr= InetAddress.getByName("localhost");
+        InetAddress _adr = InetAddress.getByName("localhost");
         this.socketUDP = new DatagramSocket();
     }
 
@@ -41,20 +49,22 @@ public class SingletonUDP
     }
 
     public String reception() throws IOException {
-        DatagramPacket recu=new DatagramPacket(new byte[_taille],_taille);;
+        DatagramPacket recu = new DatagramPacket(_buffer, _taille);
         socketUDP.receive(recu);
-        String data= new String(recu.getData());
-        System.out.println("Data recieved : " + data);
+        String data = new String(recu.getData());
+        System.out.println("Data recu : " + data + "\n");
         return data;
     }
-    public void fermetureSocket()  {
+
+    public void fermetureSocket() {
         socketUDP.close();
     }
 
+
     /** Holder */
-    private static class SingletonHolder
+    /*private static class SingletonHolder
     {
-        /** Instance unique non préinitialisée */
+        /** Instance unique non préinitialisée *//*
         private final static SingletonUDP instance;
         static {
             try {
@@ -63,11 +73,22 @@ public class SingletonUDP
                 throw new RuntimeException(e);
             }
         }
-    }
+    }*/
 
-    /** Point d'accès pour l'instance unique du singleton */
-    public static SingletonUDP getInstance()
-    {
-        return SingletonHolder.instance;
+    /**
+     * Point d'accès pour l'instance unique du singleton
+     */
+    public static SingletonUDP getInstance() throws SocketException, UnknownHostException {
+        if (_singletonUDP == null) {
+            _singletonUDP = new SingletonUDP();
+        }
+        return SingletonUDP._singletonUDP;
     }
 }
+/*
+*
+*         DatagramPacket dataSent = new DatagramPacket(data,data.length,serveur,7777 );
+        DatagramSocket socket = new DatagramSocket();
+        socket.send(dataSent);
+        DatagramPacket dataRecieved = new DatagramPacket(new byte[taille],taille);
+        socket.receive(dataRecieved);*/

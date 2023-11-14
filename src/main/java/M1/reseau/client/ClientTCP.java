e50274cd8696e0c415e89b39e1ed7a47e3414255
Victor <victorweber57@gmail.com>
2023-11-14 01:27:36 +0100

    ajout de test pour vérifer le client TCP, singleton à utiliser puis balise de test à ajouter

--- src/main/java/M1/reseau/client/ClientTCP.java
+++ src/main/java/M1/reseau/client/ClientTCP.java
@@ -6,8 +6,9 @@ import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
-    /**
+/**
      * This class implements java socket M1.reseau.ClientTCP
      * @author Malphas
      *
@@ -21,27 +22,41 @@ import java.net.UnknownHostException;
          * @throws ClassNotFoundException
          * @throws InterruptedException
          */
-        public static void main(String[] args)throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
+
+        static Scanner input=new Scanner(System.in);
+
+        public static void main(String[] args)throws IOException, ClassNotFoundException, InterruptedException{
             //get the localhost IP address, if server is running on some other IP, you need to use that
             InetAddress host = InetAddress.getLocalHost();
             Socket socket = null;
             ObjectOutputStream oos = null;
             ObjectInputStream ois = null;
-            for(int i=0; i<5;i++){
+            boolean _boucle=true;
+
+            String _message;
+            while (_boucle){
                 //establish socket connection to server
                 socket = new Socket(host.getHostName(), 9876);
                 //write to socket using ObjectOutputStream
                 oos = new ObjectOutputStream(socket.getOutputStream());
-                System.out.println("envoi du message "+i);
-                if(i==4)oos.writeObject("exit");
-                else oos.writeObject(""+i);
+                System.out.println("votre message :");
+                _message=input.nextLine();
+
+                System.out.println("envoi du message :"+_message);
+                oos.writeObject(_message);
                 //read the server response message
                 ois = new ObjectInputStream(socket.getInputStream());
-                String message = (String) ois.readObject();
-                System.out.println("Message: " + message);
-                //close resources
-                ois.close();
-                oos.close();
+                String messagereception = (String) ois.readObject();
+
+                System.out.println("Message recu: " + messagereception);
+
+                if(_message=="exit") {
+                    _boucle=false;
+                    //close resources
+                    ois.close();
+                    oos.close();
+                    socket.close();
+                }
                 Thread.sleep(100);
             }
         }

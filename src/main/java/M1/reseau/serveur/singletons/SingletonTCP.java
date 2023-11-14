1894bea2d6593477e0ad430bb2d4bc3f6a28f0f9
Victor <victorweber57@gmail.com>
2023-11-14 01:23:34 +0100

    probleme dans UDP pour l(envoi du message au serveur

--- src/main/java/M1/reseau/serveur/singletons/SingletonTCP.java
+++ src/main/java/M1/reseau/serveur/singletons/SingletonTCP.java
@@ -1,17 +1,43 @@
 package M1.reseau.serveur.singletons;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class SingletonTCP implements Serializable
 {
+    InetAddress host = InetAddress.getLocalHost();
+   //pour socket client AF
+    // Socket socket = null;
+    ObjectOutputStream oos = null;
+    ObjectInputStream ois = null;
+    private static ServerSocket serveur;
+    int port = 9876;
     /** Constructeur privé */
-    private SingletonTCP()
-    {
+    private SingletonTCP() throws IOException {
+        InetAddress host = InetAddress.getLocalHost();
+        Socket socket = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        serveur = new ServerSocket(port);
 
     }
 
     /** Instance unique pré-initialisée */
-    private static SingletonTCP INSTANCE = new SingletonTCP();
+    private static SingletonTCP INSTANCE;
+
+    static {
+        try {
+            INSTANCE = new SingletonTCP();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     /** Point d'accès pour l'instance unique du singleton */
     public static SingletonTCP getInstance()
--- src/main/java/M1/reseau/serveur/singletons/SingletonUDP.java
+++ src/main/java/M1/reseau/serveur/singletons/SingletonUDP.java
@@ -1,18 +1,68 @@
 package M1.reseau.serveur.singletons;
 
+import M1.reseau.serveur.serveur.ServeurUDP;
+
+import java.io.IOException;
+import java.net.*;
+
+
 public class SingletonUDP
 {
+
     /** Constructeur privé
      un seul socket UDP est nécessaire pour le serveur
      */
-    private SingletonUDP()
-    {}
+    final DatagramSocket socketUDP;
+    final  int _taille = 1024;
+    final byte[] _buffer = new byte[_taille];
+    final int _port;
+    InetAddress _adr ;
+    private SingletonUDP( int port,String ad) throws UnknownHostException, SocketException {
+        _port=7777;
+        InetAddress _adr= InetAddress.getByName("localhost");
+        // création d'une socket, sans la lier à un port particulier
+        this.socketUDP = new DatagramSocket();
+
+    }
+    private SingletonUDP() throws UnknownHostException, SocketException {
+        _port=7777;
+        InetAddress _adr= InetAddress.getByName("localhost");
+        this.socketUDP = new DatagramSocket();
+    }
+
+    public void message(String s) throws IOException {
+        //Message de type string deviens de type data pour l'envoi
+        byte[] data = (s).getBytes();
+        //creation du packet UDP
+        DatagramPacket packet = new DatagramPacket(data, data.length, _adr, _port);
+        // envoi du paquet via la socket PROBLEME ICI CAR SERVEUR KAPUT
+        socketUDP.send(packet);
+
+    }
+
+    public String reception() throws IOException {
+        DatagramPacket recu=new DatagramPacket(new byte[_taille],_taille);;
+        socketUDP.receive(recu);
+        String data= new String(recu.getData());
+        System.out.println("Data recieved : " + data);
+        return data;
+    }
+    public void fermetureSocket()  {
+        socketUDP.close();
+    }
 
     /** Holder */
     private static class SingletonHolder
     {
         /** Instance unique non préinitialisée */
-        private final static SingletonUDP instance = new SingletonUDP();
+        private final static SingletonUDP instance;
+        static {
+            try {
+                instance = new SingletonUDP();
+            } catch (UnknownHostException | SocketException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     /** Point d'accès pour l'instance unique du singleton */

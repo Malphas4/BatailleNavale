b5e761ba6aed9e81250dd328733eb1506fdb4186
Victor <victorweber57@gmail.com>
2023-11-15 22:05:51 +0100

    Serveur TCp de chat global + handler Va aussi être utilisé pour chat local et le jeu en se basant sur les handler dans une partie

--- src/main/java/M1/reseau/serveur/serveur/chatGlobal/ClientHandler.java
+++ src/main/java/M1/reseau/serveur/serveur/chatGlobal/ClientHandler.java
@@ -1,4 +1,84 @@
 package M1.reseau.serveur.serveur.chatGlobal;
 
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable{
+    public static ArrayList<ClientHandler> _listeClients=new ArrayList<>();
+    private Socket _socket;
+
+    //buffer a l'ancienne
+    final int _taille = 1024;
+    final byte[] _buffer = new byte[_taille];
+    //buffer recent
+    private BufferedWriter _bufferEcriture;
+    private BufferedReader _bufferLecture;
+    private String _pseudoClient;
+
+    public ClientHandler(Socket socket){
+        try{
+            _socket=socket;
+            _bufferEcriture=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            _bufferLecture=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            _pseudoClient= _bufferLecture.readLine();
+            _listeClients.add(this);
+            //broadcast("Initialiastion du chat ")
+        }catch (IOException e){
+            fermetureGlobale(_socket,_bufferEcriture,_bufferLecture);
+        }
+    }
+
+    @Override
+    public void run() {
+        String _message;
+        while(_socket.isConnected()){
+            try{
+                _message=_bufferLecture.readLine();
+                //if code broadcast
+                broadcast(_message);
+                //si code specifique
+                //                    fermetureGlobale(socket, _bufferEcriture, _bufferLecture);
+                //gestion des autres codes;
+            }catch (IOException e) {
+                fermetureGlobale(_socket, _bufferEcriture, _bufferLecture);
+            }
+        }
+    }
+    /*Fonction envoyant ubn mesage a chaque client
+    * */
+    public void broadcast(String messageEnTransit){
+        for (ClientHandler iterHandler :_listeClients){
+            try{
+                if(!iterHandler._pseudoClient.equals(_pseudoClient)) {
+                    iterHandler._bufferEcriture.write(messageEnTransit);
+                    iterHandler._bufferEcriture.newLine();
+                    iterHandler._bufferEcriture.flush();
+                }//else if code de fermeture dans le message
+                    // fermetureGlobale(socket, _bufferEcriture, _bufferLecture);
+            }catch (IOException e){
+                fermetureGlobale(_socket, _bufferEcriture, _bufferLecture);
+
+            }
+        }
+    }
+    /*Deconnexion d'un client, on retire son handler de la liste
+    * */
+    public void deconnexionClient(){
+        _listeClients.remove(this);
+    }
+    /*fermeture du socket et des bufffer pour un client
+    * */
+    public void fermetureGlobale(Socket socket,BufferedWriter _bufferEcriture,BufferedReader _bufferLecture){
+        deconnexionClient();
+        try {
+            if(_bufferEcriture !=null) _bufferEcriture.close();
+            if (_bufferLecture!=null) _bufferLecture.close();
+            if(socket !=null) socket.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- src/main/java/M1/reseau/serveur/serveur/chatGlobal/ServeurChatTCP.java
+++ src/main/java/M1/reseau/serveur/serveur/chatGlobal/ServeurChatTCP.java
@@ -1,4 +1,52 @@
 package M1.reseau.serveur.serveur.chatGlobal;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServeurChatTCP {
+
+    private ServerSocket _socketTCP;
+
+    /**Constructeur
+     *
+     ***/
+    public ServeurChatTCP(ServerSocket socket){
+        _socketTCP=socket;
+    }
+    public void demarageServeur(){
+
+        try {
+            //boucle tant que le socket est ouvert
+            while (!_socketTCP.isClosed()) {
+                Socket _socket = _socketTCP.accept();
+                System.out.print("Nouvelle connexion au serveur TCP");
+                ClientHandler _clientHandler = new ClientHandler(_socket);
+                Thread _thead = new Thread(_clientHandler);
+                _thead.start();
+
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**Fermeture du socket et ferme le serveur
+     *
+     */
+    public void fermetureServeurTCP(){
+        try {
+            if (_socketTCP !=null) _socketTCP.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    /*public static void main ...
+    ServeurSocket _socketTCP=new ServeurSocket(9732);
+    ServeurChatTCP _serveur= new ServeurChatTCP(_socketTCP);
+    _serveur.start();
+
+
+     */
+
 }

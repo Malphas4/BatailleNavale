76641c83ee295405744cfe810f3ec8f4cde2911b
RozenFR <iamzephir@gmail.com>
2023-11-25 16:08:15 +0100

    - Modification visitor et ajout medium - Modifica

--- src/main/java/M1/reseau/model/player/visitor/VisitorTirer.java
+++ src/main/java/M1/reseau/model/player/visitor/VisitorTirer.java
@@ -131,7 +131,55 @@ public class VisitorTirer implements IVisitorJoueur {
      */
     @Override
     public void visite(MediumBot _bot) throws IJoueurException {
+        /* Verification arguments non null */
+        if (get_grille() == null) throw new IllegalArgumentException("VisitorTirer : La grille ne peut pas être null.");
 
+        /* Selection d'une case random */
+        ICase _case = null;
+        do {
+            int x = 1 + (int) (Math.random() * ((get_grille().get_longueur() - 1) + 1));
+            int y = 1 + (int) (Math.random() * ((get_grille().get_largeur() - 1) + 1));
+            try {
+                _case = get_grille().get_caseParCoord(x, y);
+                if (_case instanceof CaseEtat)
+                    _case = get_grille().get_caseParCoord(x + 1, y);
+                if (_case instanceof CaseEtat)
+                    _case = get_grille().get_caseParCoord(x, y - 1);
+                if (_case instanceof CaseEtat)
+                    _case = get_grille().get_caseParCoord(x - 1, y);
+                if (_case instanceof CaseEtat)
+                    _case = get_grille().get_caseParCoord(x, y + 1);
+            } catch (IGrilleException e) {
+                throw new RuntimeException(e);
+            }
+        } while (!(_case instanceof CaseEtat));
+
+        /* Assignation de la case à tirer */
+        set_case(_case);
+
+        /* Verification que la grille contient la case */
+        if (!get_grille().isDansLaGrille(get_case())) throw new IJoueurException("VisitorTirer : La case n'est pas dans la grille.");
+
+        /* Si la case est valide */
+        if (!get_case().isTirable()) throw new IJoueurException("VisitorTirer : La case n'est pas valide.");
+
+        /* Definition de la nouvelle case en fonction de la case tirer */
+        ICase _newcase;
+        // On ajoute soit une case touchée ou soit une case ratée
+        if (get_case() instanceof CaseBateau) {
+            _newcase = new CaseTouche(get_case().get_x(), get_case().get_y());
+        } else {
+            _newcase = new CaseRate(get_case().get_x(), get_case().get_y());
+        }
+
+        /* On supprime l'ancienne case de la liste dans la grille */
+        try {
+            ICase _supCase = _bot.get_grilleTouche().get_caseParCoord(get_case().get_x(), get_case().get_y());
+            _bot.get_grilleTouche().supprimerCase(_supCase);
+            _bot.get_grilleTouche().ajouterCase(_newcase);
+        } catch (IGrilleException e) {
+            System.err.println("VisitorTirer : La case n'a pas pu être supprimé de la grille");
+        }
     }
 
     /**
--- src/main/java/M1/reseau/serveur/serveur/game/ChronoThread.java
+++ src/main/java/M1/reseau/serveur/serveur/game/ChronoThread.java
@@ -1,13 +1,14 @@
 package M1.reseau.serveur.serveur.game;
 
+import M1.reseau.model.exception.IPartieException;
+
 import java.io.IOException;
 
 public class ChronoThread extends Thread {
 
     private SalonThread _salon;
 
-    private int _playerTime1;
-    private int _playerTime2;
+    private int _time;
 
     public ChronoThread() {
 
@@ -15,6 +16,7 @@ public class ChronoThread extends Thread {
 
     public ChronoThread(SalonThread _salon) {
         set_salon(_salon);
+        set_time(30);
     }
 
     public SalonThread get_salon() {
@@ -25,37 +27,43 @@ public class ChronoThread extends Thread {
         this._salon = _salon;
     }
 
-    public int get_playerTime1() {
-        return _playerTime1;
+    public int get_time() {
+        return _time;
     }
 
-    public void set_playerTime1(int _playerTime1) {
-        this._playerTime1 = _playerTime1;
-    }
-
-    public int get_playerTime2() {
-        return _playerTime2;
-    }
-
-    public void set_playerTime2(int _playerTime2) {
-        this._playerTime2 = _playerTime2;
+    public void set_time(int _time) {
+        this._time = _time;
     }
 
     @Override
     public void run() {
         while (!isInterrupted()) {
 
-            String _message = "chrono;"
-                    + _salon.get_id()
-                    + ";" + _salon.get_j1().get_pseudo()
-                    + ";" + get_playerTime1()
-                    + ";" + _salon.get_j2().get_pseudo()
-                    + ";" + get_playerTime2();
+            String _message = null;
+            String _pseudo = null;
+
             try {
+                _pseudo = _salon.get_gameService().get_partie().getJoueurCourant().get_pseudo();
+            } catch (IPartieException e) {
+                throw new RuntimeException(e);
+            }
+
+            _message = "chrono;"
+                    + _salon.get_id()
+                    + ";" + _pseudo
+                    + ";" + get_time();
+
+            try {
+                if (get_time() <= 0) {
+                    set_time(30);
+                    _salon.get_gameService().get_partie().tourSuivant();
+                }
                 _salon.get_j1().message(_message);
                 _salon.get_j2().message(_message);
             } catch (IOException e) {
                 System.err.println("ChronoThread : Fail to send message.");
+            } catch (IPartieException e) {
+                System.err.println("ChronoThread : Fail to switch game round.");
             }
 
             try {
@@ -64,8 +72,7 @@ public class ChronoThread extends Thread {
                 System.err.println("ChronoThread : Fail to sleep.");
             }
 
-            set_playerTime1(get_playerTime1() - 1);
-            set_playerTime2(get_playerTime2() - 1);
+            set_time(get_time() - 1);
         }
     }
 }

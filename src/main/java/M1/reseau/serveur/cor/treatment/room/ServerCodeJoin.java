2c5dcf5117e3f85a69eb6638bf89755138f72791
RozenFR <iamzephir@gmail.com>
2023-11-26 18:41:33 +0100

    - Update COR

--- src/main/java/M1/reseau/serveur/cor/treatment/room/ServerCodeJoin.java
+++ src/main/java/M1/reseau/serveur/cor/treatment/room/ServerCodeJoin.java
@@ -20,7 +20,7 @@ public class ServerCodeJoin extends ServerCOR {
      */
     @Override
     public void execute(String _message, SalonThread _salon) {
-        System.out.println("|\tInput message : join salon");
+        System.out.println("> ServerCodeJoin : join salon");
         GameService _gameService = _salon.get_gameService();
 
         String[] _sp = _message.split(";");
@@ -31,6 +31,7 @@ public class ServerCodeJoin extends ServerCOR {
             else if (_salon.get_j2() == null) _salon.set_j2(_sp[2]);
             else throw new IPartieException("ServerCodeJoin : Error");
 
+            System.out.println("\t> Joueur " + _sp[2] + " a ete ajoute.");
             // Add player in the game
             _gameService.get_partie().ajouterJoueur(_sp[2]);
 
@@ -52,7 +53,7 @@ public class ServerCodeJoin extends ServerCOR {
         } catch (IOException e) {
             System.err.println("ServerCodeJoin : Error sending message.");
         }
-        System.out.println("|\tInput message : Fin join salon");
+        System.out.println("> ServerCodeJoin : Fin join salon");
     }
 
     /**
@@ -62,6 +63,7 @@ public class ServerCodeJoin extends ServerCOR {
     @Override
     public boolean isMessageCorrect(String _message) {
         String[] _sp = _message.split(";");
+        System.out.println("> ServerCodeJoin : " + _sp[0] + " - " + _sp[0].equalsIgnoreCase("join"));
         return _sp[0].equalsIgnoreCase("join");
     }
 }

0aabfa3799483dca5bb745c6aebba4b10a75bc9a
RozenFR <iamzephir@gmail.com>
2023-11-26 16:37:19 +0100

    - Modification code et comment chrono

--- src/main/java/M1/reseau/serveur/cor/treatment/game/ServerCodeTourSuivant.java
+++ src/main/java/M1/reseau/serveur/cor/treatment/game/ServerCodeTourSuivant.java
@@ -1,32 +1,39 @@
 package M1.reseau.serveur.cor.treatment.game;
 
 import M1.reseau.model.exception.IPartieException;
+import M1.reseau.model.player.classic.JoueurNormal;
 import M1.reseau.serveur.cor.ServerCOR;
 import M1.reseau.serveur.serveur.game.GameService;
 import M1.reseau.serveur.serveur.game.SalonThread;
 
 import java.io.IOException;
 
-public class ServerCodeCommencer extends ServerCOR {
+public class ServerCodeTourSuivant extends ServerCOR {
 
-    public ServerCodeCommencer() {
+    public ServerCodeTourSuivant() {
 
     }
 
     /**
-     * Receive : commencer;[salon id]
+     * Receive : tour suivant;[salon id];[joueur]
      * @param _message
      * @param _salon
      */
     @Override
     public void execute(String _message, SalonThread _salon) {
         GameService _gameService = _salon.get_gameService();
+
+        String[] _sp = _message.split(";");
+
         try {
-            _gameService.get_partie().commencer();
             String _jc = _gameService.get_partie().getJoueurCourant().get_pseudo();
-            _salon.get_j1().message("commencer;" + _salon.get_id() + _jc);
-            _salon.get_j2().message("commencer;" + _salon.get_id() + _jc);
-            _salon.get_chrono().set_time(30);
+            if (_jc.equalsIgnoreCase(_sp[2])) {
+                _gameService.get_partie().tourSuivant();
+                _jc = _gameService.get_partie().getJoueurCourant().get_pseudo();
+                _salon.get_j1().message("tour suivant;" + _salon.get_id() + _jc);
+                _salon.get_j2().message("commencer;" + _salon.get_id() + _jc);
+                _salon.get_chrono().set_time(30);
+            }
         } catch (IPartieException | IOException e) {
             System.err.println("ServerCodeCommencer : La partie ne peut pas commencer.");
         }
@@ -39,6 +46,6 @@ public class ServerCodeCommencer extends ServerCOR {
     @Override
     public boolean isMessageCorrect(String _message) {
         String[] _sp = _message.split(";");
-        return _sp[0].equals("commencer");
+        return _sp[0].equals("tour suivant");
     }
 }
